55f225a652fc24d927613237cb02732d3b4b60f5
Jialong Yang <yangjialong.me@gmail.com>
2020-03-08 17:04:15 +0800

    Initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    title: 'Codeages SpringBootSkeleton 二次开发指南',
+    description: 'Codeages SpringBootSkeleton 二次开发指南',
+    themeConfig: {
+        sidebar: [
+          {
+            title: '基本概念',   // 必要的
+            children: [
+              '/'
+            ]
+          },
+          {
+            title: '快速入门',
+            children: [ /* ... */ ]
+          },
+          {
+            title: '开发指南',
+            children: [ /* ... */ ]
+          },
+          {
+            title: '示例',
+            children: [ /* ... */ ]
+          }
+        ]
+      }
+  }
